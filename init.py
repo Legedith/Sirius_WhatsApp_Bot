62c7dae80beaded5058ee3c6719b6a6d4a4654e7
Vipul-Bajaj <vipulbjj@gmail.com>
2020-10-09 01:26:21 +0530

    Fixed Crucio command and formatted code

--- init.py
+++ init.py
@@ -30,6 +30,8 @@ random_response = False
 
 msg_box = driver.find_element_by_class_name(message_box)
 mode = "message-out"
+
+
 def main():
     global target
     global random_response
@@ -42,22 +44,22 @@ def main():
             lold = len(cmnd)
             clean = decoded(cmnd[-1])
             if clean[0].lower() != phase.lower():
-                    continue
-            execute(clean[1:-2])
+                continue
+            execute(clean[1:-1])
             target = False
-            lnew=lold
+            lnew = lold
             continue
-            
+
         lnew = len(cmnd)
         if lold < lnew:
-            print(lnew-lold)
-            excmd = cmnd[-(lnew-lold):]
+            print(lnew - lold)
+            excmd = cmnd[-(lnew - lold):]
             for c in excmd:
                 clean = decoded(c)
                 if clean[0].lower() != phase.lower():
                     continue
-                execute(clean[1:-2])
-            lold=lnew
+                execute(clean[1:-1])
+            lold = lnew
 
         if random_response:
             lnew_in = len(driver.find_elements_by_class_name("message-in"))
@@ -74,100 +76,102 @@ def execute(cmd):
     global target
     global random_response
     if cmd[0] == 'spam' or cmd[0] == 'crucio':
-        if(len(cmd) > 2):
+        if (len(cmd) > 2):
             spam(cmd[1:])
         else:
-            msg_box.send_keys('Crucio failed'+Keys.RETURN)
-            
+            msg_box.send_keys('Crucio failed due to wrong command format.\n '
+                              'Usage: {0} crucio/spam <your spam message> <number of times to '
+                              'be spammed>\n Ex: {0} crucio Hi!There!!! 5'.format(phase) + Keys.RETURN)
+
     elif cmd[0] == 'spam.big' or cmd[0] == 'sectumsempra':
-        if(len(cmd)>3):
+        if (len(cmd) > 3):
             focus(cmd[1])
             spam(cmd[2:])
             focus(name)
         else:
-            msg_box.send_keys('Sectum Sempra failed'+Keys.RETURN)
-            
+            msg_box.send_keys('Sectum Sempra failed' + Keys.RETURN)
+
     elif cmd[0] == 'change' or cmd[0] == 'apparate':
         target = True
-        if(len(cmd)>1):
+        if (len(cmd) > 1):
             name = cmd[1]
             focus(name)
-            msg_box.send_keys("Things just got a hell lot more interesting... "+Keys.RETURN)
+            msg_box.send_keys("Things just got a hell lot more interesting... " + Keys.RETURN)
         else:
-            msg_box.send_keys("Couldn't Apparate, Focus harder"+Keys.RETURN)
-            
+            msg_box.send_keys("Couldn't Apparate, Focus harder" + Keys.RETURN)
+
     elif cmd[0] == 'rename' or cmd[0] == 'disapparate':
-        if(len(cmd)>1):
+        if (len(cmd) > 1):
             phase = cmd[1]
         else:
-            msg_box.send_keys("Couldn't Disapparate, Focus harder"+Keys.RETURN)
-    
+            msg_box.send_keys("Couldn't Disapparate, Focus harder" + Keys.RETURN)
+
     elif cmd[0] == 'mode' or cmd[0] == 'snape':
         target = True
-        if(len(cmd)>1):
+        if (len(cmd) > 1):
             name = cmd[1]
             focus(name)
-            if(cmd[1] == 'in'):
+            if (cmd[1] == 'in'):
                 mode = 'message-in'
             else:
                 mode = 'message-out'
-            msg_box.send_keys("Things just got a hell lot more interesting... "+Keys.RETURN)
-            
+            msg_box.send_keys("Things just got a hell lot more interesting... " + Keys.RETURN)
+
         else:
-            msg_box.send_keys("Couldn't Disapparate, Focus harder"+Keys.RETURN)
-            
+            msg_box.send_keys("Couldn't Disapparate, Focus harder" + Keys.RETURN)
+
     elif cmd[0] == 'google' or cmd[0] == 'alohomora':
-        if(len(cmd)>1):
+        if (len(cmd) > 1):
             subject = ' '.join(cmd[1:])
-            msg_box.send_keys('https://www.google.com/search?q='+removeSpaces(subject)+Keys.RETURN)
+            msg_box.send_keys('https://www.google.com/search?q=' + removeSpaces(subject) + Keys.RETURN)
         else:
-            msg_box.send_keys("No doors to open!"+Keys.RETURN)
+            msg_box.send_keys("No doors to open!" + Keys.RETURN)
 
     elif cmd[0] == 'urban':
-        if(len(cmd)>1):
+        if (len(cmd) > 1):
             subject = ' '.join(cmd[1:])
             search = urban_dict(removeSpaces(subject))
-            msg_box.send_keys(search+Keys.RETURN)
+            msg_box.send_keys(search + Keys.RETURN)
         else:
-            msg_box.send_keys("Hear, Hear!"+Keys.RETURN)
+            msg_box.send_keys("Hear, Hear!" + Keys.RETURN)
 
     elif cmd[0] == 'wiki':
-        if(len(cmd)>1):
+        if (len(cmd) > 1):
             subject = ''.join(cmd[1:])
             search = wiki(removeSpacesWiki(subject))
-            msg_box.send_keys(search+Keys.RETURN)
+            msg_box.send_keys(search + Keys.RETURN)
         else:
-            msg_box.send_keys("Hear, Hear!"+Keys.RETURN)
-    
+            msg_box.send_keys("Hear, Hear!" + Keys.RETURN)
+
     elif cmd[0] == 'youtube' or cmd[0] == 'accio':
-        if(len(cmd)>1):
+        if (len(cmd) > 1):
             subject = ' '.join(cmd[1:])
             link = youtube(subject)
             msg_box.send_keys(link)
-#            time.sleep(2)
+            #            time.sleep(2)
             msg_box.send_keys(Keys.ENTER)
         else:
-            msg_box.send_keys("Concentrate harder on the object!"+Keys.RETURN)
-    
+            msg_box.send_keys("Concentrate harder on the object!" + Keys.RETURN)
+
     elif cmd[0] == 'soundcloud' or cmd[0] == 'cantis':
-        if(len(cmd)>1):
+        if (len(cmd) > 1):
             subject = ' '.join(cmd[1:])
             link = soundcloud(subject)
             msg_box.send_keys(link)
-#            time.sleep(2)
+            #            time.sleep(2)
             msg_box.send_keys(Keys.ENTER)
         else:
-            msg_box.send_keys("Concentrate harder on the object!"+Keys.RETURN)
+            msg_box.send_keys("Concentrate harder on the object!" + Keys.RETURN)
 
     elif cmd[0] == 'response':
         random_response = True
         if len(cmd) > 1 and cmd[1] == 'stop':
             random_response = False
-    
+
     else:
-        msg_box.send_keys('Wrong Incantation'+Keys.RETURN)
-  
-    
+        msg_box.send_keys('Wrong Incantation' + Keys.RETURN)
+
+
 def youtube(subject):
     d = webdriver.Chrome()    
     d.get('https://www.youtube.com/results?search_query='+removeSpaces(subject))
@@ -175,6 +179,7 @@ def youtube(subject):
     link = vid.get_attribute('href')
     d.close()
     return link
+
 
 def soundcloud(subject):
     d = webdriver.Chrome()    
@@ -184,13 +189,15 @@ def soundcloud(subject):
     d.close()
     return link
 
+
 def urban_dict(query):
     d = webdriver.Chrome()
     d.get('https://www.urbandictionary.com/define.php?term='+removeSpaces(query))
     meanings = d.find_elements_by_class_name('meaning')
-    meaning  = ' '.join([meaning.text for meaning in meanings])
+    meaning = ' '.join([meaning.text for meaning in meanings])
     d.close()
     return meaning
+
 
 def wiki(query):
     d = webdriver.Chrome()
@@ -205,20 +212,31 @@ def wiki(query):
 
 
 def spam(cmd):
-    text = ' '.join(cmd[:-1])
-    for i in range(int(cmd[-1])):
-                msg_box.send_keys(text+Keys.RETURN)
+    spam_message = ' '.join(cmd[:-1]) + '\n'
+    try:
+        iterations = int(cmd[-1])
+    except ValueError:
+        msg_box.send_keys("Invalid value of iteration. Last value of the command should be a valid integer.\n")
+        return
+    for _ in range(iterations):
+        msg_box.send_keys(spam_message)
+        time.sleep(0.75)
+
+
 def decoded(c):
     return c.text.split()
 
+
 def removeSpaces(s):
-    return s.replace(" ",'+')
+    return s.replace(" ", '+')
+
 
 def removeSpacesWiki(s):
     """
     Wikipedia uses underscores instead of '+' on their links
     """
-    return s.replace(" ",'_')    
+    return s.replace(" ", '_')
+
 
 def focus(name):
     global search
@@ -229,15 +247,16 @@ def focus(name):
     search.send_keys(Keys.RETURN)
     msg_box = driver.find_element_by_class_name(message_box)
 
+
 def random_response_gen():
-    responses = ["Okay", "OK", "Okk", "Ohk", "lol", "Nice", "Hmmm", "Noice", "Maybe", "No", "Yes", "Yeah", "No way", "Sweet"]
+    responses = ["Okay", "OK", "Okk", "Ohk", "lol", "Nice", "Hmmm", "Noice", "Maybe", "No", "Yes", "Yeah", "No way",
+                 "Sweet"]
     return random.choice(responses)
+
 
 if __name__ == "__main__":
     while True:
         try:
             main()
-        except:
-            print('got an error here!') #+ Exception
-        
-        
+        except Exception as e:
+            print('got an error here! {}'.format(e))  # + Exception
